4fa880b95ab30ab255f32c4cf33b5e325a842a8b
Anton Gorokh <rerearitet@gmail.com>
2023-05-25 01:11:06 +0200

    local file storage implementation

---
+++ src/test/kotlin/com/devformed/sickgame/TestConfiguration.kt
@@ -0,0 +1,10 @@
+package com.devformed.sickgame
+
+import org.springframework.context.annotation.ComponentScan
+
+/**
+ * @author Anton Gorokh
+ */
+@ComponentScan("com.devformed.sickgame")
+class TestConfiguration {
+}
